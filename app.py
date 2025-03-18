2ce1276f889a980b9cbe3ecbb8c95cb2fe11d828
Paulo Santangelo <paulocbsantangelo@gmail.com>
2025-03-18 09:49:44 -0300

    Corrigindo carregamento de vídeos

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route('/static/videos/<path:filename>')
 def serve_video(filename):
-    return send_from_directory('static/videos', filename)
+    return send_from_directory("static/videos", filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
